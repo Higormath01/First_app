e70c4fe3e0af867ab75cb62d969cc5749ed2f08e
Higormath01 <higormatheus579@gmail.com>
2025-05-16 09:09:48 -0300

    a

--- exercicios/livroslistview.py
+++ exercicios/livroslistview.py
@@ -4,10 +4,11 @@ from flet.core.colors import Colors
 from flet.core.textfield import TextField
 
 class User:
-    def __init__(self, nome, salario,profissoes):
-        self.nome = nome
-        self.salario = salario
-        self.profissoes = profissoes
+    def __init__(self, titulo, descricao,categoria, autor):
+        self.titulo = titulo
+        self.descricao = descricao
+        self.categoria = categoria
+        self.autor = autor
 
 
 def main(page: ft.Page):
@@ -20,45 +21,47 @@ def main(page: ft.Page):
     # Funções
     lista = []
 
-    def salvar_nome(e):
-        if input_nome.value == "" or input_salario.value == "" or input_profissoes.value == "":
+    def salvar_titulo(e):
+        if input_titulo.value == "" or input_descricao.value == "" or input_categoria.value == "" or input_autor.value == "":
             page.overlay.append(msg_error)
             msg_error.open = True
             page.update()
 
         else:
             obj_user = User(
-                nome=input_nome.value,
-                salario=input_salario.value,
-                profissoes=input_profissoes.value,
+                titulo=input_titulo.value,
+                descricao=input_descricao.value,
+                categoria=input_categoria.value,
+                autor=input_autor.value,
             )
             lista.append(obj_user)
-            input_salario.value = ""
-            input_nome.value = ""
-            input_profissoes.value = ""
+            input_descricao.value = ""
+            input_titulo.value = ""
+            input_categoria.value = ""
+            input_autor.value = ""
 
             page.overlay.append(msg_sucesso)
             msg_sucesso.open = True
             page.update()
 
-    # def salvar_nome(e):
-    #     if input_nome.value == "":
+    # def salvar_titulo(e):
+    #     if input_titulo.value == "":
     #         page.overlay.append(msg_error)
     #         msg_error.open = True
     #         page.update()
     #
     #     else:
     #         lista.append(obj_user)
-    #         input_nome.value = ""
+    #         input_titulo.value = ""
     #         page.overlay.append(msg_sucesso)
     #         msg_sucesso.open = True
     #         page.update()
 
     def exibir_lista(e):
-        lv_nome.controls.clear()
+        lv_titulo.controls.clear()
         for user in lista:
-            lv_nome.controls.append(
-                ft.Text(value=f"{user.nome} - {user.salario} - {user.profissoes}" ),
+            lv_titulo.controls.append(
+                ft.Text(value=f"{user.titulo} - {user.descricao} - {user.categoria} - {user.autor}" ),
             )
         page.update()
 
@@ -71,12 +74,14 @@ def main(page: ft.Page):
                 "/",
                 [
                     AppBar(title=Text("Home"), bgcolor=Colors.PRIMARY_CONTAINER),
-                    input_nome,
-                    input_salario,
-                    input_profissoes,
+                    input_titulo,
+                    input_descricao,
+                    input_categoria,
+                    input_autor,
+
                     ft.Button(
                         text="Salvar",
-                        on_click=lambda _: salvar_nome(e),
+                        on_click=lambda _: salvar_titulo(e),
 
                     ),
                     ft.Button(
@@ -94,9 +99,10 @@ def main(page: ft.Page):
                     "/segunda",
                     [
                         AppBar(title=Text("Segunda tela"), bgcolor=Colors.SECONDARY_CONTAINER),
-                        lv_nome,
-                        lv_salario,
-                        lv_profissoes
+                        lv_titulo,
+                        lv_descricao,
+                        lv_categoria,
+                        lv_autor,
 
                     ],
                 )
@@ -110,26 +116,29 @@ def main(page: ft.Page):
 
     # Componentes
     msg_sucesso = ft.SnackBar(
-        content=ft.Text("nome salvo com sucesso!"),
+        content=ft.Text("titulo salvo com sucesso!"),
         bgcolor=Colors.GREEN
     )
 
     msg_error = ft.SnackBar(
-        content=ft.Text(("O nome nao pode estar vazio!"),
+        content=ft.Text(("O titulo nao pode estar vazio!"),
                         bgcolor=Colors.RED)
     )
 
-    input_nome = ft.TextField(label="Nome")
-    input_salario = ft.TextField(label="Salario")
-    input_profissoes = ft.TextField(label="Profissoes")
+    input_titulo = ft.TextField(label="titulo")
+    input_descricao = ft.TextField(label="descricao")
+    input_categoria = ft.TextField(label="categoria")
+    input_autor = ft.TextField(label="autor")
 
-    lv_nome = ft.ListView(
+    lv_titulo = ft.ListView(
         height=500)
 
-    lv_profissoes = ft.ListView()
+    lv_categoria = ft.ListView()
 
-    lv_salario = ft.ListView(
+    lv_descricao = ft.ListView(
     )
+
+    lv_autor = ft.ListView()
 
     # Eventos
     page.on_route_change = gerencia_rotas
